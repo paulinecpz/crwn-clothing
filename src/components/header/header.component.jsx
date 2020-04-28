0cc4104c258bd9d56f110d834564030910e89297
Pauline Coupez <coupez.pauline@gmail.com>
2020-04-28 16:48:24 +0100

    update redux for user"

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { connect } from 'react-redux'
+import { connect } from "react-redux";
 
 import { Link } from "react-router-dom";
 
@@ -34,6 +34,6 @@ const Header = ({ currentUser }) => (
 );
 
 const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser
-})
-export default connect(mapStateToProps) Header;
+  currentUser: state.user.currentUser,
+});
+export default connect(mapStateToProps)(Header);
